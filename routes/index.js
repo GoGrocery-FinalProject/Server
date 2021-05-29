542aaa3a4e046ac61a4aef4a761f6357c75242c7
ahmad-hanafi <57187761+ahmad-hanafi@users.noreply.github.com>
2021-05-29 12:14:28 +0700

    misahin route biar rapi

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
 const router = require('express').Router()
-const User = require('../controllers/userController')
-const Product = require('../controllers/productController')
-const {authenticate, adminAuthorize, customerAuthorize} = require('../middlewares/auth.js')
+const userRoutes = require('./userRoute')
+const productRoutes = require('./productRoute')
+const {authenticate } = require('../middlewares/auth.js')
 
-
-router.use('/register', User.register)
-router.use('/login', User.login)
-
-router.get('/products', Product.showAll) 
-router.get('/products/:id', Product.findById)
-
+router.use(userRoutes)
 router.use(authenticate)
-
-router.post('/products', adminAuthorize, Product.addProduct) 
-router.put('/products/:id', adminAuthorize, Product.putUpdate) 
-router.patch('/products/:id', adminAuthorize, Product.patchUpdate) 
-router.delete('/products/:id', adminAuthorize, Product.delete) 
+router.use('/products',productRoutes)
 
 module.exports = router
---
+++ routes/productRoute.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const Product = require('../controllers/productController')
+const { adminAuthorize } = require('../middlewares/auth.js')
+
+
+router.get('/', Product.showAll) 
+router.get('/:id', Product.findById)
+
+
+router.post('/', adminAuthorize, Product.addProduct) 
+router.put('/:id', adminAuthorize, Product.putUpdate) 
+router.patch('/:id', adminAuthorize, Product.patchUpdate) 
+router.delete('/:id', adminAuthorize, Product.delete)
+
+module.exports = router
