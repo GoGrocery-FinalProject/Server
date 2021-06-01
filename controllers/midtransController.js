05a37070c2bf2f4a86f52f55b6da5ed09e64af96
farhantaufiks <farhantaufiks@gmail.com>
2021-06-02 06:33:55 +0700

    bug fixing for webhooks notification handler midtrans

--- controllers/midtransController.js
+++ controllers/midtransController.js
@@ -1,19 +1,22 @@
 const midtransClient = require('midtrans-client')
-const { Transaction } = require('../models')
+const { Transaction, Product } = require('../models')
 const axios = require('axios')
 
+//flow midtrans ketika transakis dibuat inpput 2 kali post database intern dan post database midtrans
+//pakai webhooks > sediain endpoint
+//ada di dashboard midtrans => notifcation url di masukan endpointnya
+
 class MidtransController {
-	static pay(req, res, next) {
-		let link
+	static pay(req, res) {
 		let snap = new midtransClient.Snap({
 			isProduction: false,
-			serverKey: 'SB-Mid-server-49N3DSl3CDLcYTrkloIpfJsm',
-			clientKey: 'SB-Mid-client-PCbLw1JpX0QdlFiK',
+			serverKey: 'SB-Mid-server-OkJLecqkB5bPgBQhcPsJCKWY',
+			clientKey: 'SB-Mid-client-sW5AHuqn__lVIlq3',
 		})
 
 		let parameter = {
 			transaction_details: {
-				order_id: 'ORDER-101-' + Math.round(new Date().getTime() / 1000),
+				order_id: Math.round(new Date().getTime() / 1000),
 				gross_amount: req.body.gross_amount,
 			},
 			item_details: req.body.item_details,
@@ -23,25 +26,19 @@ class MidtransController {
 		}
 
 		snap.createTransaction(parameter).then((transaction) => {
-			link = transaction.redirect_url
-			return Transaction.create({
-				UserId: +req.body.userId,
-				products: JSON.stringify(parameter.item_details),
-				order_id: parameter.transaction_details.order_id,
-				totalPrice: parameter.transaction_details.gross_amount,
-			})
+			// send transaction token && link
+			let link = transaction.redirect_url
+			let transactionToken = transaction.token
+			let clientKey = snap.apiConfig.clientKey
+			res.status(200).json({ link, transactionToken, clientKey })
 		})
-		.then(() => {
-			res.status(200).json({ link, order_id:  parameter.transaction_details.order_id})
-		})
-		.catch((err) => {
-			console.log(err)
+		.catch(err => {
 			next(err)
 		})
-
 	}
 
-	static checkStatus(req, res, next) {
+	//diganti endpoint baru untuk handle notif dari midtrans (webhooks)
+	static checkStatus(req, res) {
 		axios({
 			url: `https://api.sandbox.midtrans.com/v2/${req.body.order_id}/status`,
 			method: 'GET',
@@ -50,12 +47,13 @@ class MidtransController {
 				Accept: 'application/json',
 				Authorization:
 					'Basic ' +
-					Buffer.from('SB-Mid-server-49N3DSl3CDLcYTrkloIpfJsm').toString(
+					Buffer.from('SB-Mid-server-OkJLecqkB5bPgBQhcPsJCKWY').toString(
 						'base64'
 					),
 			},
 		})
 			.then(({ data }) => {
+				console.log(data)
 				res.status(200).json(data)
 			})
 			.catch((err) => {
@@ -63,52 +61,41 @@ class MidtransController {
 			})
 	}
 
-	static NotificationHandler(req, res) {
-		let order_id = req.body.order_id
-		let transactionStatus = req.body.transaction_status
+	static NotificationHandler(req, res, next) {
+    let order_id = req.body.order_id;
+    let transactionStatus = req.body.transaction_status;
 
-		if (transactionStatus == 'settlement') {
-			Transaction.findOne({ where: { order_id: order_id } })
-				.then((data) => {
-					if (data) {
-						let products = JSON.parse(data.products)
-						products.forEach((el) => {
-							let newStock
-							products.findOne({ where: { id: el.id } }).then((data) => {
-								newStock = data.stock - el.quantity
-								Product.update(
-									{ stock: newStock },
-									{
-										where: {
-											id: +data.id,
-										},
-									}
-								)
-							})
-						})
-						Transaction.update(
-							{ status: 'paid' },
-							{ where: { order_id: order_id } }
-						).then(() => {
-							res.status(200).JSON({ ok: 'OK' })
-						})
-					} else {
-						res
-							.status(404)
-							.JSON({ status: 'notfound', message: 'try again 2 times' })
-					}
-				})
-				.catch((err) => {
-					next(err)
-				})
-		} else {
-			if (transactionStatus === 'pending') {
-				res.status(200)
-			} else {
-				next({name: 'internal server error'})
-			}
-		}
-	}
+    if (transactionStatus == 'settlement'){
+      Transaction.findOne({ where: { order_id: order_id } })
+        .then((data) => {
+          if(data){
+            let products = JSON.parse(data.products)
+            products.forEach(el => {
+              let newStock
+              Product.findOne({ where: { id: el.id }})
+                .then((data) => {
+                  newStock = data.stock - el.quantity
+                  Product.update({ stock: newStock }, {
+                    where: {
+                      id: +data.id
+                    }
+                  })
+                })
+            })
+            Transaction.update({ status: "paid"}, { where: { order_id: order_id }})
+              .then(() => {
+                res.status(200).json({ ok: 'OK' })
+              })
+          } else {
+            res.status(404).json({ status: "notfound", message: "try again 2 times"})
+          }
+        })
+        .catch((err) => {
+					console.log(err.message)
+          next(err)
+        })
+    }
+  }
 }
 
 module.exports = MidtransController
