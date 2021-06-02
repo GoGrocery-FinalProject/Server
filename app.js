b478da742b2dcb0590c9e9ba20756df722f492c6
farhantaufiks <farhantaufiks@gmail.com>
2021-06-02 11:33:44 +0700

    before

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-
 const router = require('./routes/index')
 const express = require('express')
 const cors = require('cors')
